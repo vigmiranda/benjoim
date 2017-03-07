3bb61e24dc357d88f82c0aa7597b2f5291ce42b8
vigmiranda <vigmiranda@gmail.com>
2017-03-07 19:56:50 -0300

    Ajuste no Server

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app     = express();
 
-app.get('/', function (req, res) {
-  res.send('Server Executando');
-})
+app.set('port', (process.env.PORT || 5000));
 
-app.get('/login', function (req, res) {
-  res.send('logado');
-})
-
-app.get('/user', function (req, res) {
-  res.send('usuario');
-})
-
-
-app.listen(5000, function () {
-  console.log('Example app listening on port 3000!')
-})
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
