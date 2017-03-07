4c640ae29aa7b77f5bf4f038ba6951c80cfded99
vigmiranda <vigmiranda@gmail.com>
2017-03-07 18:01:40 -0300

    Server

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Server Executando');
+})
+
+app.get('/login', function (req, res) {
+  res.send('logado');
+})
+
+app.get('/user', function (req, res) {
+  res.send('usuario');
+})
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
