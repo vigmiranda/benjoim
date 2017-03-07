2cbe522dea252ebe5c8555690d8657b193d7011d
vigmiranda <vigmiranda@gmail.com>
2017-03-07 20:09:42 -0300

    Ajuste no Server

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ app.set('port', (process.env.PORT || 5000));
 
 //For avoidong Heroku $PORT error
 app.get('/', function(request, response) {
-    var result = 'App is running'
+    var result = 'Server rodando.'
     response.send(result);
-}).listen(app.get('port'), function() {
+})
+.get('/login', function(request, response){
+    var result = 'Login'
+    response.send(result);	
+})
+.listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
